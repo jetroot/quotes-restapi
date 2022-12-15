03ea730ef5a56a4ef89adacc0a5f5ebcaebc371f
hashgit <abdrazakghiyati2@gmail.com>
2022-12-14 23:11:52 -0800

    sql commands to execute

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- Insert roles
+INSERT INTO roles (role_name) VALUES('ROLE_USER');
+INSERT INTO roles (role_name) VALUES('ROLE_ADMIN');
+
+-- Insert Categories
+INSERT INTO categories (category_name) VALUES('Love');
+INSERT INTO categories (category_name) VALUES('Sport');
+INSERT INTO categories (category_name) VALUES('Music');
